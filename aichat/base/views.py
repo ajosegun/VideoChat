9bcee853318086fc4acccc22dcdd202932d68f33
Olusegun Ajose <ajoseolusegun@gmail.com>
2022-08-15 10:24:47 +0200

    Token generation

--- aichat/base/views.py
+++ aichat/base/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
 
 # Create your views here.
+
+def getToken(request):
+    #Build token with uid
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
 
 def lobby(request):
     return render(request, 'base/lobby.html')
--- aichat/static/js/streams.js
+++ aichat/static/js/streams.js
@@ -1,6 +1,6 @@
 const APP_ID = '29f40bf594ee417cabbd74e1fc43b36b'
 const CHANNEL = 'main'
-const TOKEN = '00629f40bf594ee417cabbd74e1fc43b36bIAAJY901V6Xsol1E7mTynkPVN48scYLUeuZREgrNzpkuJGTNKL8AAAAAEACLq5A0Ac/6YgEAAQABz/pi'
+const TOKEN = '00629f40bf594ee417cabbd74e1fc43b36bIAAKBIYeVRl7//3BCIAlaea6mCDw0awozliHFtWgIn8WbGTNKL8AAAAAEACLq5A0BNr6YgEAAQAE2vpi/6YgEAAQABz/pi'
 let UID;
 
 const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
@@ -9,17 +9,97 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
+    // Join the channel
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
+    // Get audio and video tracks
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
+    // Create a player
     let player = `<div class="video-container" id="user-container-${UID}"> 
                         <div class="username-wrapper"><span class="user-name">My Name</span></div>
                         <div class="video-player" id="user-${UID}"></div>
                     </div> `
 
+    // Append the player to a video stream
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
-    localTracks[1].play()
+    // Play it
+    localTracks[1].play(`user-${UID}`)
+
+    // Publish the track
+    await client.publish([localTracks[0], localTracks[1]])
 
 }
+
+let handleUserJoined = async (user, mediaType) => {
+    remoteUsers[user.UID] = user
+    await client.subscribe(user, mediaType)
+
+    if(mediaType === 'video'){
+        let player = document.getElementById(`user-container=${user.UID}`)
+
+        if (player != null){
+            player.remove()
+        }
+    }
+
+    // Create a player
+    let player = `<div class="video-container" id="user-container-${user.UID}"> 
+                        <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                        <div class="video-player" id="user-${user.UID}"></div>
+                    </div> `
+
+    // Append the player to a video stream
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    user.videoTrack.play(`user-${user.UID}`)
+
+    if(mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+} 
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.UID]
+    document.getElementById(`user-container-${user.UID}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+    for (let i=0; localTracks>length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+
+    await client.leave()
+    window.open('/', '_self')
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+
+}
+
+let toggleMic = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+
+}
+
+joinAndDisplayLocalStream()
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
